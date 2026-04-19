f85b4aef599a8170a9d74a43b7e45fd15b4f3219
Ayesha <Ayesha@LAPTOP-QTJ9CBH4>
2026-04-19 21:33:10 +0800

    First Commit

---
+++ src/main/java/com/mycompany/expensestracker/ExpensesTracker.java
@@ -0,0 +1,14 @@
+package com.mycompany.expensestracker;
+
+/**
+ *
+ * @author Ayesha P. Caragay
+ * Team: MEOW..
+ */
+public class ExpensesTracker {
+
+    public static void main(String[] args) {
+        com.formdev.flatlaf.FlatLightLaf.setup(); // Or FlatDarkLaf
+        new MainFrame().setVisible(true);
+    }
+}
